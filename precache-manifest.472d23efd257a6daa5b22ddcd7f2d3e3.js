335bf7dacc467ca14bd28f9b6c13bcad666d9793
AbhishekSunda <abhisheksunda@outlook.com>
2020-09-07 14:12:38 +0530

    Updates

---
+++ precache-manifest.472d23efd257a6daa5b22ddcd7f2d3e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b5c7d740243fb3cab3fef4618dac91d",
+    "url": "/FaceRecogonition/index.html"
+  },
+  {
+    "revision": "2c0f7071237c8e227074",
+    "url": "/FaceRecogonition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "cf313649e9178984a246",
+    "url": "/FaceRecogonition/static/css/main.713dd9ac.chunk.css"
+  },
+  {
+    "revision": "2c0f7071237c8e227074",
+    "url": "/FaceRecogonition/static/js/2.5f2fe3da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FaceRecogonition/static/js/2.5f2fe3da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf313649e9178984a246",
+    "url": "/FaceRecogonition/static/js/main.4efcea9d.chunk.js"
+  },
+  {
+    "revision": "ee9864ceaf3faa95a7f2",
+    "url": "/FaceRecogonition/static/js/runtime-main.bb9d3264.js"
+  }
+]);
