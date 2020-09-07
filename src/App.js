40eb98aee9530db4ec59fb564e0b289edb2bc444
AbhishekSunda <51685152+AbhishekSunda@users.noreply.github.com>
2020-09-07 14:10:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,147 @@
+import React, {Component} from 'react';
+import './App.css';
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
+import Clarifai from 'clarifai';
+import FaceRecogonition from './Components/FaceRecogonition/FaceRecogonition';
+import SignIn from './Components/SignIn/SignIn';
+import Register from './Components/Register/Register';
+
+
+
+const app = new Clarifai.App({
+ apiKey: 'eeec217ffd084fcca63e70982a7ac59a'
+});
+
+class App extends Component{
+	constructor(){
+		super();
+		this.state={
+			input:'',
+			imageUrl:'https://media.glamour.com/photos/569579a35fff94d44eebf3cf/master/pass/beauty-2014-02-13-spring-beauty-vacation-glow-main.jpg',
+			clarifai_data:[],
+			isSignedIn:false,
+			route:'signin',
+		}
+	}
+
+addBorders=()=>{
+		const x =document.getElementById('inputImage');
+		const height=Number(x.height)
+		const width=Number(x.width)
+		const y=document.getElementById('boundingBox');
+		
+	Element.prototype.remove = function() {
+    this.parentElement.removeChild(this);
+	}
+	NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+    for(var i = this.length - 1; i >= 0; i--) {
+        if(this[i] && this[i].parentElement) {
+            this[i].parentElement.removeChild(this[i]);
+        }
+    }
+}
+document.getElementsByClassName("boxingModel").remove();
+
+
+	for(let i=0;i<this.state.clarifai_data.length;++i){
+		
+		const z= document.createElement('div');
+		z.classList.add("boxingModel");
+		
+		
+		z.style.top=(this.state.clarifai_data[i].top_row*height)+"px";
+		z.style.bottom=(height-this.state.clarifai_data[i].bottom_row*height)+"px";
+		z.style.left=(this.state.clarifai_data[i].left_col*width)+"px";
+		z.style.right=(width-this.state.clarifai_data[i].right_col*width)+"px";
+		y.appendChild(z);
+	}
+}
+
+onInputChange=(event)=>{
+	this.setState({
+		input:event.target.value
+	})
+}
+
+onSubmit=(event) =>{
+
+	this.setState({
+		imageUrl:this.state.input,
+	})
+
+const array=[]
+
+app.models.predict(Clarifai.FACE_DETECT_MODEL,
+ 		 this.state.input)
+        .then(response => {
+       		
+       		this.array=response.rawData.outputs[0].data.regions;
+       		
+       		this.state.clarifai_data=this.array.map((obj)=>{
+       			return obj.region_info.bounding_box;
+       		})
+ 
+       		
+         this.addBorders();
+          
+        })
+        .catch(err => {
+          console.log(err);
+        });
+
+
+
+   }
+
+  onRouteChange=(route)=>{
+  	if(route==='home'){
+  		this.setState({
+  			isSignedIn:true,
+  		})
+  	}
+  	if(route==='signin'){
+  		this.setState({
+  			isSignedIn:false,
+  		})
+  	}
+
+  	this.setState({route:route});
+  }
+  render(){
+    return (
+    	<div >
+    	<Navigation onRouteChange={this.onRouteChange} isSignedIn={this.state.isSignedIn}/>
+    	{	(this.state.route!=='home')?(
+    		this.state.route==='signin'?
+    		<SignIn onRouteChange={this.onRouteChange}/>:
+    		<Register onRouteChange={this.onRouteChange}/>
+    	
+
+    		)
+    	:
+    	          
+    	          <div>
+
+    	          <Logo/>
+    	          <Rank/>
+    	          <ImageLinkForm 
+    	          onInputChange={this.onInputChange}
+    	          onSubmit={this.onSubmit}
+    	          />
+    	          <FaceRecogonition 
+    	           imageUrl={this.state.imageUrl}
+    	           />
+
+    	           </div>
+
+    	       }
+        </div>
+
+        );
+  }
+}
+
+export default App;
---
+++ src/Components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Rank = () => {
+	return(
+		<div>
+		<div className='flex justify-center pa3'>
+		{`your rank is ..`}
+		</div>
+		<div className="flex justify-center">
+		#5
+		</div>
+		</div>
+		)
+}
+
+export default Rank;
